6ea05fce5e9bd3e21f9ffaab1a2efe3ea5476e30
mjkaufer <mjkaufer@gmail.com>
2015-01-28 09:53:55 -0500

    Push sounds to fb

--- SoundShare/app/src/main/java/org/kaufer/soundshare/Share.java
+++ SoundShare/app/src/main/java/org/kaufer/soundshare/Share.java
@@ -7,28 +7,55 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+
+import java.util.Date;
+import java.util.HashMap;
 
 
 public class Share extends Activity {
 
     Button share;
     Spinner genre;
-
+    EditText artist, song;
+    Firebase ref = new Firebase("https://kaufersoundshare.firebaseio.com/");
+    Firebase soundRef = ref.child("sounds");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
         Firebase.setAndroidContext(this);
+
         share = (Button)findViewById(R.id.share);
+        artist = (EditText)findViewById(R.id.artist);
+        song = (EditText)findViewById(R.id.song);
+
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //make a new Sound, send to firebase
+            CharSequence genreErr = "Please choose a genre";
+            if(genre.getSelectedItem().toString().equals("Genre"))
+                Toast.makeText(getApplicationContext(), "Please choose a genre", Toast.LENGTH_SHORT).show();
+            else if(song.getText().toString().length() == 0)
+                Toast.makeText(getApplicationContext(), "Please enter a song", Toast.LENGTH_SHORT).show();
+            else if(artist.getText().toString().length() == 0)
+                Toast.makeText(getApplicationContext(), "Please enter an artist", Toast.LENGTH_SHORT).show();
+            else{//everything checks out
+                Sound share = new Sound(song.getText().toString(), artist.getText().toString(), genre.getSelectedItem().toString());//change to string values
+                HashMap<String, Sound> sounds = new HashMap<String, Sound>();
+                sounds.put(new Date().getTime() + "", share);
+                soundRef.push().setValue(sounds);//push to FB
 
-                Sound share = new Sound("","","");//change to string values
+            }
+
+
             }
         });
         genre = (Spinner)findViewById(R.id.genre);
